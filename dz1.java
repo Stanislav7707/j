a4f8c294accae7bc43a1c8eb0afc93fc8625bee0
Stanislav7707 <98480413+Stanislav7707@users.noreply.github.com>
2022-01-31 16:20:54 +0200

    dz1

---
+++ dz1.java
@@ -0,0 +1,49 @@
+{\rtf1\ansi\ansicpg1251\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 Menlo-BoldItalic;\f2\fswiss\fcharset0 Helvetica;
+}
+{\colortbl;\red255\green255\blue255;\red217\green232\blue247;\red204\green108\blue29;\red230\green230\blue250;
+\red18\green144\blue195;\red249\green250\blue244;\red30\green181\blue64;\red121\green171\blue255;\red242\green242\blue0;
+\red167\green236\blue33;\red141\green218\blue248;\red23\green198\blue163;\red243\green236\blue121;}
+{\*\expandedcolortbl;;\csgenericrgb\c85098\c90980\c96863;\csgenericrgb\c80000\c42353\c11373;\csgenericrgb\c90196\c90196\c98039;
+\csgenericrgb\c7059\c56471\c76471;\csgenericrgb\c97647\c98039\c95686;\csgenericrgb\c11765\c70980\c25098;\csgenericrgb\c47451\c67059\c100000;\csgenericrgb\c94902\c94902\c0;
+\csgenericrgb\c65490\c92549\c12941;\csgenericrgb\c55294\c85490\c97255;\csgenericrgb\c9020\c77647\c63922;\csgenericrgb\c95294\c92549\c47451;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf2  \cf3 import\cf2  java\cf4 .\cf2 util\cf4 .\cf2 Scanner\cf4 ;\cf0 \
+\
+\
+\cf3 public\cf2  \cf3 class\cf2  \cf5 DZ1\cf2  \cf6 \{\cf0 \
+\cf2     \cf3 public\cf2  \cf3 static\cf2  \cf3 void\cf2  \cf7 main\cf6 (\cf5 String\cf6 []\cf2  \cf8 args\cf6 )\cf2  \cf6 \{\cf0 \
+\
+\
+\cf2         \cf5 Scanner\cf2  \cf9 scanner\cf2  \cf4 =\cf2  \cf3 new\cf2  \cf10 Scanner\cf6 (\cf5 System\cf4 .
+\f1\i\b \cf11 in
+\f0\i0\b0 \cf6 )\cf4 ;\cf0 \
+\
+\cf2         \cf5 System\cf4 .
+\f1\i\b \cf11 out
+\f0\i0\b0 \cf4 .\cf10 println\cf6 (\cf12 "What is your name? "\cf6 )\cf4 ;\cf0 \
+\
+\cf2         \cf5 String\cf2  \cf9 name\cf2  \cf4 =\cf2  \cf13 scanner\cf4 .\cf10 nextLine\cf6 ()\cf4 ;\cf0 \
+\cf2         \cf5 System\cf4 .
+\f1\i\b \cf11 out
+\f0\i0\b0 \cf4 .\cf10 println\cf6 (\cf12 "Where are you live,"\cf2  \cf4 +\cf2  \cf13 name\cf6 )\cf4 ;\cf0 \
+\cf2         \cf0 \
+\cf2         \cf5 String\cf2  \cf9 name1\cf2  \cf4 =\cf2  \cf13 scanner\cf4 .\cf10 nextLine\cf6 ()\cf4 ;\cf0 \
+\cf2         \cf5 System\cf4 .
+\f1\i\b \cf11 out
+\f0\i0\b0 \cf4 .\cf10 println\cf6 (\cf12 "How old are you?"\cf2  \cf4 +\cf2  \cf13 name1\cf6 )\cf4 ;\cf0 \
+\
+\
+\
+\cf2         \cf5 String\cf2  \cf9 address\cf2  \cf4 =\cf2  \cf13 scanner\cf4 .\cf10 nextLine\cf6 ()\cf4 ;\cf0 \
+\cf2         \cf5 System\cf4 .
+\f1\i\b \cf11 out
+\f0\i0\b0 \cf4 .\cf10 println\cf6 (\cf12 "My name is "\cf2  \cf4 +\cf2  \cf13 name1\cf2  \cf4 +\cf2  \cf12 ", "\cf2  \cf4 +\cf2  \cf12 " I am "\cf2  \cf4 +\cf2  \cf12 " I live in "\cf2  \cf4 +\cf2  \cf13 address\cf6 )\cf4 ;\cf0 \
+\
+\cf2     \cf6 \}\cf0 \
+\cf6 \}
+\f2 \cf0 \
+}
